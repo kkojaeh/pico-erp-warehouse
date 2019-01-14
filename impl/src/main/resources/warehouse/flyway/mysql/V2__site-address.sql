301e988fb783a539b30d9af3bc858f51e4f312fd
Ethan <kkojaeh@gmail.com>
2019-01-14 13:36:39 +0900

    DDL 추가

---
+++ impl/src/main/resources/warehouse/flyway/mysql/V2__site-address.sql
@@ -0,0 +1,3 @@
+ALTER TABLE wah_warehouse_site ADD address_detail varchar(50);
+ALTER TABLE wah_warehouse_site ADD address_postal_code varchar(10);
+ALTER TABLE wah_warehouse_site ADD address_street varchar(50);
